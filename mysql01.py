c550522f22292f478443d09d2d554af955835f43
kokot300 <kokot300@gmail.com>
2020-03-30 17:34:16 +0200

    Update mysql01.py

--- mysql01.py
+++ mysql01.py
@@ -5,4 +5,18 @@ conn=mysql.connector.connect(host='localhost',database='mydb',user='root',passwo
 if conn.is_connected():
     print("connected")
 
+cursor=conn.cursor()
+cursor.execute('select * from emp')
+cursor.execute('insert into emp values(3,'joe',30000)')
+#row=cursor.fetchone() #fetch one with while or fetch all with for
+row=cursor.fetchall()
+print("total no of records ", cursor.rowcount)
+
+for row in rows:
+    print(row)
+#while row is not None:
+#    print(row)
+#    row=cursor.fetchone()
+
+cursor.close()
 conn.close()
